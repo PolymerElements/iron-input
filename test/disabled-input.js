baa8ed67394b7c0faadded5406b463e8083c37dc
Elliott Marquez <emarquez@google.com>
2018-06-22 17:04:40 -0700

    auto-converted by polymer-modulizer

--- test/disabled-input.js
+++ test/disabled-input.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,21 +6,23 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<link rel="import" href="../../polymer/polymer.html">
-<link rel="import" href="../../iron-validator-behavior/iron-validator-behavior.html">
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+Polymer({
+  _template: html`
+    <iron-input bind-value="{{myValue}}" invalid="{{myInvalid}}" id="input">
+      <input disabled="">
+    </iron-input>
+`,
 
-<script>
-  Polymer({
+  is: 'disabled-input',
 
-    is: 'letters-only',
+  properties: {
+    myValue: {value: 'foo'},
 
-    behaviors: [Polymer.IronValidatorBehavior],
-
-    validate: function(value) {
-      return !value || value.match(/^[a-zA-Z]*$/) !== null;
-    }
-
-  });
-</script>
+    myInvalid: {value: false}
+  }
+});
--- test/letters-only.js
+++ test/letters-only.js
@@ -1,4 +1,4 @@
-<!--
+/**
 @license
 Copyright (c) 2015 The Polymer Project Authors. All rights reserved.
 This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
@@ -6,25 +6,19 @@ The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
 The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
 Code distributed by Google as part of the polymer project is also
 subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+*/
+import '@polymer/polymer/polymer-legacy.js';
 
-<link rel="import" href="../../polymer/polymer.html">
+import { IronValidatorBehavior } from '@polymer/iron-validator-behavior/iron-validator-behavior.js';
+import { Polymer } from '@polymer/polymer/lib/legacy/polymer-fn.js';
+Polymer({
 
-<dom-module id="disabled-input">
-  <template>
-    <iron-input bind-value="{{myValue}}" invalid="{{myInvalid}}" id="input">
-      <input disabled>
-    </iron-input>
-  </template>
-</dom-module>
+  is: 'letters-only',
 
-<script>
-  Polymer({
-    is: 'disabled-input',
-    properties: {
-      myValue: {value: 'foo'},
+  behaviors: [IronValidatorBehavior],
 
-      myInvalid: {value: false}
-    }
-  });
-</script>
+  validate: function(value) {
+    return !value || value.match(/^[a-zA-Z]*$/) !== null;
+  }
+
+});
